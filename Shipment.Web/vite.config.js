e7e6e3b012ac4e8277a7e5f074362dd0f6122156
Mustafa Arıkçı <arikci@gmail.com>
2022-08-13 01:19:30 +0300

    Added initial web app commit

---
+++ Shipment.Web/vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  filenameHashing:true,
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./app', import.meta.url))
+    }
+  },
+  build: {
+    outDir: './wwwroot/dist',
+    rollupOptions: {
+			output: {
+				entryFileNames: 'app.js',
+				assetFileNames: 'app.css',
+			}
+		}
+  }
+})
